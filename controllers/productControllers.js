3b0e5fa9f15825e84a59046b16b63d777f84bfd8
Jayanthi Balasubramani <jayanthibs@gmail.com>
2026-03-16 14:16:31 -0400

    updated controllers.js logic

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,68 +1,80 @@
 import Product from "../models/Product.js";
 
-export async function createProduct(req, res){
-    try{
-    const newProduct = Product.create(req.body);
+export async function createProduct(req, res) {
+  try {
+    const newProduct = await Product.create(req.body);
     console.log(newProduct);
     res.status(201).json(newProduct);
-    } catch(error){
-        res.status(400).json({message:error.message});
-    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
-export  async function getProduct(req, res){
-    try{
-        const {id} = req.params;
-        const product = Product.findById(id);
-        console.log(product);
-        res.status(200).json(product);
-
-    }catch(error){
-        res.status(404).json({message:`Product with ${id} is not found`});
-    }
+export async function getProduct(req, res) {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    console.log(product);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(404).json({ message: `Product with ${id} is not found` });
+  }
 }
 
-export  async function updateProduct(req, res){
-    try{
-        const {id} = req.params;
-        const options = {
-            new: true
-        }
-        const updateProduct = Product.findByIdAndUpdate(id, req.body, options);
-        console.log(updateProduct);
-        res.status(201).json(updateProduct);
-
-    }catch(error){
-        res.status(404).json({message:`Product with ${id} is not found`})
-    }
+export async function updateProduct(req, res) {
+  try {
+    const { id } = req.params;
+    const options = {
+      new: true,
+    };
+    const updateProduct = await Product.findByIdAndUpdate(
+      id,
+      req.body,
+      options,
+    );
+    console.log(updateProduct);
+    res.status(201).json(updateProduct);
+  } catch (error) {
+    res.status(404).json({ message: `Product with ${id} is not found` });
+  }
 }
 
-
-export  async function deleteProduct(req, res){
-    try{
-        const {id} = req.params;
-        const deletedProduct = Product.findByIdAndDelete(id);
-        if(!deleteProduct){
-            res.status(404).json({message:`Product with ${id} is not found`})
-        }else{
-        console.log(deletedProduct);
-        res.status(200).json(deletedProduct);
-        }
-
-    }catch(error){
-         res.status(400).json({message:error.message})
+export async function deleteProduct(req, res) {
+  try {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    if (!deleteProduct) {
+      res.status(404).json({ message: `Product with ${id} is not found` });
+    } else {
+      console.log(deletedProduct);
+      res.status(200).json(deletedProduct);
     }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
+export async function getProducts(req, res) {
+  try {
+    const { category, minPrice, maxPrice, sortBy } = req.query;
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const products = await Product.find({
+      $or: [
+        { category: category },
+        { minPrice: minPrice },
+        { maxPrice: maxPrice },
+      ]
+    })
+      .sort({ price: sortBy })
+      .skip((page - 1) * limit)
+      .limit(limit);
 
-export  async function getProducts(req, res){
-    try{
-        const {category, minPrice, maxPrice, sortBy, page, limit} = req.query;
-        const product = Product.findById(id);
-        console.log(product);
-        res.status(200).json(product);
 
-    }catch(error){
-        res.status(404).json({message:`Product with ${id} is not found`});
-    }
+    console.log(products);
+    res.status(200).json(products);
+
+  } catch (error) {
+    res.status(404).json({ message: `Product with ${id} is not found` });
+  }
 }
